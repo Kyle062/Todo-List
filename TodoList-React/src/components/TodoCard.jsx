bd0c0636e830cabb367b095bc78a24cf3da059c2
Kyle062 <kylealba0624@gmail.com>
2025-03-17 02:27:04 -0800

    Initial commit

---
+++ TodoList-React/src/components/TodoCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function TodoCard(props) {
+  const { children, handleDeleteTodos, handleEditTodos, index } = props;
+  return (
+    <li className="todoItem">
+      {children}
+      <div className="actionsContainer">
+        <button
+          onClick={() => {
+            handleEditTodos(index);
+          }}
+        >
+          <i class="fa-solid fa-pen-to-square"></i>
+        </button>
+        <button onClick={() => handleDeleteTodos(index)}>
+          <i class="fa-solid fa-trash"></i>
+        </button>
+      </div>
+    </li>
+  );
+}
